065f17ce1d1422c85a80056be1fbfff429ea25c5
Rishabh Singh <rishabhjuly11@gmail.com>
2023-06-17 14:55:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
-import Header from "./components/Header"
 import '../src/CSS_Files/App.css'
+import Header from "./components/Header"
+import Meme from "./components/Meme"
 
 export default function App() {
     return (
         <div>
             <Header />
+            <Meme />
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from "react"
-import '../images/troll-face.png'
 
 export default function Header() {
     return (
         <header className="header">
             <img 
-                src="../images/troll-face.png" 
+                src="./images/troll-face.png" 
                 className="header--image"
+                alt="img"
             />
             <h1 className="header--title">Meme Generator</h1>
             <h4 className="header--project">React Course - Project 3</h4>
