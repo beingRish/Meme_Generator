bfb53eab9386c02d45ea6bf16936a3aabbe98d48
Rishabh Singh <rishabhjuly11@gmail.com>
2023-06-21 12:17:37 +0530

    update useState method to the Meme Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,6 @@ import React from "react"
 export default function Header() {
     return (
         <header className="header">
-            <img 
-                src="./images/troll-face.png" 
-                className="header--image"
-                alt="img"
-            />
             <h1 className="header--title">Meme Generator</h1>
             <h4 className="header--project">React Course - Project 3</h4>
         </header>
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,27 +1,36 @@
-import React from "react";
-import memesData from "../memesData";
+import React from "react"
+import memesData from "../memesData.js"
 
-export default function Meme(){
+export default function Meme() {
 
-    const [memeImage, setMemeImage] = React.useState("")
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
-    function getMemeImage(){
-        const memesArray = memesData.data.memes
+
+    function getMemeImage() {
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
 
     }
 
-    return(
+    return (
         <main>
             <div className="form">
-                <input 
-                    type="text" 
+                <input
+                    type="text"
                     placeholder="Top text"
                     className="form--input"
-
                 />
-                <input 
+                <input
                     type="text"
                     placeholder="Bottom text"
                     className="form--input"
@@ -30,10 +39,10 @@ export default function Meme(){
                     className="form--button"
                     onClick={getMemeImage}
                 >
-                    Get a new meme image🖼️
+                    Get a new meme image 🖼
                 </button>
             </div>
-            <img src={memeImage} className="meme--image" alt="img"/>
+            <img src={meme.randomImage} className="meme--image" />
         </main>
     )
 }
