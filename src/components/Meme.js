e247c8f70d3f0ffa7f20fbc10bfd61dd6bd60d58
Rishabh Singh <rishabhjuly11@gmail.com>
2023-07-02 19:31:10 +0530

    Added Text to Image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -19,7 +19,14 @@ export default function Meme() {
             ...prevMeme,
             randomImage: url
         }))
+    }
 
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name] : value
+        }))
     }
 
     return (
@@ -29,11 +36,17 @@ export default function Meme() {
                     type="text"
                     placeholder="Top text"
                     className="form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
                 />
                 <input
                     type="text"
                     placeholder="Bottom text"
                     className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
                 />
                 <button
                     className="form--button"
@@ -42,7 +55,11 @@ export default function Meme() {
                     Get a new meme image 🖼
                 </button>
             </div>
-            <img src={meme.randomImage} className="meme--image" />
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }
